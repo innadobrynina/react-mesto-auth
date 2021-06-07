4249bca3511fe00e5a06eca2802bb0442c54f53f
Inna Dobrynina <a-ya-dobraya@yandex.ru>
2021-06-07 07:21:07 +0500

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <p className="footer__author">&copy; 2021 Mesto Russia</p>
-        </footer>
-    );
-}
-
-export default Footer;
